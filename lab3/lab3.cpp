b1c0aceacf4fc373d589f9f1e506b188c3500841
yaigoryao <petrochenkov.igor2015@yandex.ru>
2024-03-07 18:57:04 +0300

    Добавьте файлы проекта.

---
+++ lab3/lab3.cpp
@@ -0,0 +1,597 @@
+﻿#include <iostream>
+#include <string>
+#include <list>
+#include <algorithm>
+#include <stack>
+#include <functional>
+#define _CRTDBG_MAP_ALLOC
+#include <stdlib.h>
+#include <crtdbg.h>
+/*Класс Double Linked List*/
+template <class T>
+class DLL
+{
+public:
+    class Item
+    {
+    public:
+        T _value;
+        Item* prev;
+        Item* next;
+        /*prev = next = nullptr*/
+        Item() : prev{ nullptr }, next{ nullptr } {};
+        /*_value = value
+        prev = next = nullptr*/
+        Item(T value) : Item(), _value{ value } {};
+        /*_value = value
+        prev = prev_
+        next = next_*/
+        Item(T value, Item* prev_, Item* next_) : _value{ value }, prev{ prev_ }, next{ next_ } {};
+    };
+protected:
+    /*Класс Item элементы DLL*/
+    struct DLLIter
+    {
+        explicit DLLIter(Item* ptr) : _ptr{ptr} {};
+        T& operator*() const { return this->_ptr->_value; };
+        T* operator->() const { return &(this->_ptr->_value); };
+        DLLIter& operator++() { this->_ptr = this->_ptr->next; return *this; };
+        DLLIter& operator++(int) { auto ret = *this; this->_ptr = this->_ptr->next; return ret; };
+        DLLIter& operator--() { this->_ptr = this->_ptr->prev; return *this; };
+        DLLIter& operator--(int) { auto ret = *this; this->_ptr = this->_ptr->prev; return ret; };
+        friend bool operator == (const DLLIter& first, const DLLIter& second) { return first._ptr == second._ptr; }
+        friend bool operator != (const DLLIter& first, const DLLIter& second) { return first._ptr != second._ptr; }
+    private:
+        Item* _ptr;
+    };
+
+    struct const_DLLIter
+    {
+        explicit const_DLLIter(Item* ptr) : _ptr{ ptr } {};
+        const_DLLIter(const const_DLLIter& it) : _ptr{ it._ptr } {};
+        T& operator*() const { return this->_ptr->_value; };
+        T* operator->() const { return &(this->_ptr->_value); };
+        const_DLLIter& operator++() { this->_ptr = this->_ptr->next; return *this; };
+        const_DLLIter& operator++(int) { auto ret = *this; this->_ptr = this->_ptr->next; return ret; };
+        const_DLLIter& operator--() { this->_ptr = this->_ptr->prev; return *this; };
+        const_DLLIter& operator--(int) { auto ret = *this; this->_ptr = this->_ptr->prev; return ret; };
+        friend bool operator == (const const_DLLIter& first, const const_DLLIter& second) { return first._ptr == second._ptr; }
+        friend bool operator != (const const_DLLIter& first, const const_DLLIter& second) { return first._ptr != second._ptr; }
+    private:
+        Item* _ptr;
+    };
+
+    Item* head = nullptr;
+    Item* tail = nullptr;
+    size_t _length = 0;
+public:
+
+    DLLIter begin() noexcept { return DLLIter(this->head); };
+    DLLIter end() noexcept { return DLLIter(this->tail == nullptr? nullptr : this->tail->next); };
+
+    const_DLLIter cbegin() const noexcept { return const_DLLIter(this->head); };
+    const_DLLIter cend() const noexcept { return const_DLLIter(this->tail == nullptr? nullptr: this->tail->next); };
+
+    DLL() : head{ nullptr }, tail{ nullptr }, _length{ 0 } {/* std::cout << "Construct: " << this << std::endl;*/ };
+
+    DLL(std::initializer_list<T> li)
+    {
+        /*std::cout << "Construct: " << this << std::endl;*/
+        auto begin = li.begin();
+        while (begin != li.end())
+        {
+            this->emplace_back(*begin);
+            begin++;
+        }
+    }
+    ~DLL()
+    {
+        if (this->_length > 0)
+        {
+            /*std::cout << "Destroy: " << this << std::endl;*/
+            Item* cur = this->head;
+            Item* next = nullptr;
+            while (cur != nullptr)
+            {
+                next = cur->next;
+                delete cur;
+                cur = next;
+            }
+            this->head = this->tail = nullptr;
+            this->_length = 0;
+        }
+        /*delete cur;*/
+    }
+
+    DLL(const DLL& copy)
+    {
+        const_DLLIter begin = copy.cbegin();
+        const_DLLIter end = copy.cend();
+        while (begin != end)
+        {
+            this->emplace_back(*begin);
+            begin++;
+        }
+    }
+    
+    DLL& operator=(const DLL& copy)
+    {
+        const_DLLIter begin = copy.cbegin();
+        const_DLLIter end = copy.cend();
+        while (begin != end)
+        {
+            this->emplace_back(*begin);
+            begin++;
+        }
+        return *this;
+    }
+
+    DLL(DLL&& other)
+    {
+        this->head = other.head;
+        this->tail = other.tail;
+        this->_length = other._length;
+        other._length = 0;
+    }
+    void emplace_back(T value)
+    {
+        if (!this->head) { this->head = new Item(value, nullptr, nullptr); }
+        else if (!this->tail)
+        {
+            this->tail = new Item(value, this->head, nullptr);
+            this->head->next = this->tail;
+        }
+        else
+        {
+            Item* cur = new Item(value, this->tail, nullptr);
+            this->tail->next = cur;
+            this->tail = cur;
+        }
+        this->_length++;
+    }
+
+    /*void for_each(void(*fun)(T _value) mutable)
+    {
+        if (this->_length > 0)
+        {
+            Item* cur = this->head;
+            while (cur != this->tail)
+            {
+                fun(cur->_value);
+                cur = cur->next;
+            }
+            fun(cur->_value);
+        }
+    }*/
+    void for_each(std::function<void(T _value)> fun)
+    {
+        if (this->_length > 0)
+        {
+            Item* cur = this->head;
+            while (cur != this->tail)
+            {
+                fun(cur->_value);
+                cur = cur->next;
+            }
+            fun(cur->_value);
+        }
+    }
+
+    void remove_back()
+    {
+        if (this->_length == 0) return;
+        else if (this->_length > 2)
+        {
+            Item* remove = this->tail;
+            this->tail = this->tail->prev;
+            delete remove;
+            this->tail->next = nullptr;
+        }
+        else if (this->_length == 2)
+        {
+            delete this->tail;
+            this->tail = nullptr;
+        }
+        else
+        {
+            delete this->head;
+            this->head = nullptr;
+        }
+        this->_length--;
+    }
+
+    void remove_front()
+    {
+        if (this->_length == 0) return;
+        else if (this->_length > 2)
+        {
+            Item* remove = this->head;
+            this->head = this->head->next;
+            this->head->prev = nullptr;
+            delete remove;
+        }
+        else if (this->_length == 2)
+        {
+            delete this->head;
+            this->head = this->tail;
+            this->head->prev = nullptr;
+            this->tail = nullptr;
+            this->head->next = this->tail;
+        }
+        else
+        {
+            delete this->head;
+            this->head = nullptr;
+        }
+        this->_length--;
+    }
+
+    size_t length() const { return this->_length; }
+
+    bool empty() const { return this->_length == 0; }
+    void emplace_front(T value)
+    {
+        if (!this->head)
+        {
+            this->head = new Item(value, nullptr, nullptr);
+        }
+        else if (!this->tail)
+        {
+            this->tail = new Item(value, nullptr, this->head);
+            this->head->prev = this->tail;
+            Item* temp = this->head;
+            this->head = this->tail;
+            this->tail = temp;
+        }
+        else
+        {
+            Item* cur = new Item(value, nullptr, this->head);
+            this->head->prev = cur;
+            this->head = cur;
+        }
+        this->_length++;
+    }
+
+    /*bool any_of(void (*func)())
+    {
+        if (this->_length > 0)
+        {
+            Item* cur = this->head;
+            while (cur != this->tail)
+            {
+                if (fun(cur->_value)) return true;
+                cur = cur->next;
+            }
+            if (fun(cur->_value)) return true;
+        }
+        return false;
+    }*/
+    bool any_of(std::function<bool(T _value)> fun)
+    {
+        if (this->_length > 0)
+        {
+            Item* cur = this->head;
+            while (cur != this->tail)
+            {
+                if (fun(cur->_value)) return true;
+                cur = cur->next;
+            }
+            if (fun(cur->_value)) return true;
+        }
+        return false;
+    }
+    T front()
+    {
+        if (this->head) return this->head->_value;
+    }
+    T back()
+    {
+        if (this->tail) return this->tail->_value;
+        else if (this->head) return this->head->_value;
+    }
+    T& operator[] (int index)
+    {
+        if (index >= 0 && index <= this->_length)
+        {
+            Item* cur = this->head;
+            for (int i = 0; i < index; i++)
+            {
+                cur = cur->next;
+            }
+            return cur->_value;
+        }
+    }
+};
+
+template<class T>
+class Stack
+{
+private:
+    DLL<T> _container;
+public:
+    template< class T>
+    friend class DLL;
+    Stack(const DLL<T>& container) { this->_container = container; }
+    void push(T value) { _container.emplace_front(value); }
+    T top() { return _container.front(); }
+    T pop()
+    {
+        T temp = _container.front();
+        _container.remove_front();
+        return temp;
+    }
+    bool empty(){ return _container.empty(); }
+    size_t count() { return _container.length(); }
+    DLL<T>& get_container() { return this->_container; }
+};
+
+template<class T>
+class Queue
+{
+private:
+    DLL<T> _container;
+public:
+    template< class T>
+    friend class DLL;
+    Queue(const DLL<T>& container) { this->_container = container; }
+    void push(T value) { _container.emplace_front(value); }
+    T last() { return _container.back(); }
+    T shift()
+    {
+        T temp = _container.back();
+        _container.remove_back();
+        return temp;
+    }
+    bool empty() { return _container.empty(); }
+    size_t count() { return _container.length(); }
+    DLL<T>& get_container() { return this->_container; }
+};
+
+class Person
+{
+private:
+    std::string _surname = "";
+    std::string _patronymics = "";
+    std::string _name = "";
+    std::string _job = "";
+public:
+    Person() : _surname{ "" }, _patronymics{ "" }, _name{ "" }, _job{""}{};
+    Person(std::string name, std::string surname, std::string patronymics, std::string job) :
+        _name{ name }, _surname{ surname }, _patronymics{ patronymics }, _job{ job }{};
+    std::string getJob() const { return this->_job; };
+    std::string getFullName() const { return (this->_name + " " + this->_surname + " " + this->_patronymics); }
+    friend std::ostream& operator<< (std::ostream& os, const Person& person)
+    {
+        return os << person.getFullName();
+    }
+};
+
+std::list<Person> matchPersonProfessions(std::list<Person> people, std::list<std::string> professions)
+{
+    std::list<Person> filtered = std::list<Person>();
+    std::for_each(people.begin(), people.end(), [&filtered, professions](Person person)
+        {
+            if (std::any_of(professions.begin(), professions.end(), [person](std::string profession) {return person.getJob() == profession; }))
+            {
+                filtered.emplace_back(person);
+            }
+        });
+    return filtered;
+}
+
+DLL<Person> matchPersonProfessions(DLL<Person> people, DLL<std::string> professions)
+{
+    /*DLL<Person> filtered = DLL<Person>();
+    std::for_each(people.begin(), people.end(), [&filtered, &professions](Person person)
+        {
+            if (std::any_of(professions.begin(), professions.end(), [person](std::string profession) {return person.getJob() == profession; }))
+            {
+                filtered.emplace_back(person);
+            }
+        });
+    return filtered;*/
+    DLL<Person> filtered = DLL<Person>();
+    people.for_each([&filtered, &professions](Person person)->void 
+        {
+            if (professions.any_of([&person](std::string profession) {return person.getJob() == profession; }))
+            {
+                filtered.emplace_back(person);
+            }
+        } );
+    return filtered;
+}
+
+bool openSk(char sk)
+{
+    if (sk == '(' || sk == '[' || sk == '{') return true;
+    else return false;
+}
+
+bool closeSk(char sk)
+{
+    if (sk == ')' || sk == ']' || sk == '}') return true;
+    else return false;
+}
+
+//bool isValid(std::string skobki)
+//{
+//    if (skobki.size() % 2 != 0) return false;
+//    std::stack<char> sequence = std::stack<char>();
+//    for (auto el : skobki)
+//    {
+//        if (openSk(el)) sequence.push(el);
+//        else if (closeSk(el))
+//        {
+//            if (sequence.empty() || sequence.top() != oppositeSk(el)) return false;
+//            else sequence.pop();
+//        }
+//    }
+//    return true;
+//}
+
+char oppositeSk(char sk)
+{
+    switch (sk)
+    {
+    case '(':
+        return ')';
+        break;
+    case '[':
+        return ']';
+        break;
+    case '{':
+        return '}';
+        break;
+    case ')':
+        return '(';
+        break;
+    case ']':
+        return '[';
+        break;
+    case '}':
+        return '{';
+        break;
+    }
+}
+
+bool isValid(std::string skobki)
+{
+    if (skobki.size() % 2 != 0) return false;
+    Stack<char> sequence = Stack<char>(DLL<char>());
+    for (auto el : skobki)
+    {
+        if (openSk(el)) sequence.push(el);
+        else if (closeSk(el))
+        {
+            if (sequence.empty() || sequence.top() != oppositeSk(el)) return false;
+            else sequence.pop();
+        }
+    }
+    return true;
+}
+
+template<class T1, class T2>
+struct Tuple
+{
+public:
+    T1 _value1;
+    T2 _value2;
+    Tuple(T1 v1 = {}, T2 v2 = {}) _value1 { v1 }, _value2{ v2 } {};
+};
+
+Tuple<int, int> segmentWhereMinMultMaxGreatest(Queue<int> values, int length)
+{
+    if (length > values.count()) return Tuple<int, int>(-1, -1);
+    int localmx = -INT_MAX;
+    int localmn = INT_MAX;
+    for (int i = 0; i < length; i++)
+    {
+        int cur = values.shift();
+        if (cur > localmx) localmx = cur;
+        else if (cur < localmn) localmn = cur;
+    }
+    int mult = -INT_MAX;
+    while (values.count())
+    {
+        int localmult = localmx * localmn;
+        if (localmult > mult) mult = localmult;
+        /*int localmult = localmx * localmn;
+        if (localmult > mult) mult = localmult;
+        int cur = values.shift();
+        if (cur > localmx) localmx = cur;
+        else if (cur < localmn) localmn = cur;*/
+    }
+};
+
+int main(int argc, char** argv)
+{
+    setlocale(0, "ru");
+    //DLL<int> list = {-2, -1, 0};
+    //std::for_each(list.begin(), list.end(), [](DLL<int>::Item el) {std::cout << el._value << std::endl; });
+    /*list.for_each([](int &value) {std::cout << value << std::endl; });
+    std::cout << "===========================" << std::endl;
+    list.remove_front();
+    list.remove_front();
+    list.remove_front();
+    list.for_each([](int& value) {std::cout << value << std::endl; });*/
+    //list.remove_back();
+    //list.remove_back();
+    //list.remove_back();
+    //list.remove_back();
+    //list.for_each([](int& value) {std::cout << value << std::endl; });
+
+
+
+
+    ////1 
+    DLL<Person> employee = {
+        Person("Архип", "Архипов", "Архипович", "Слесарь"),
+        Person("Борис", "Борисов", "Борисович", "Директор"),
+        Person("Валентин", "Валентинов", "Валентинович", "Повар"),
+        Person("Геннадий", "Геннадьев", "Геннадьевич", "Сварщик"),
+        Person("Дмитрий", "Дмитриев", "Дмитриевич", "Повар")
+    };
+    DLL<std::string> jobs = {
+        "Повар",
+        "Директор"
+    };
+    auto filtered = matchPersonProfessions(employee, jobs);
+    for (auto& person : filtered) { std::cout << person << std::endl; } 
+    
+
+
+
+    //std::list <Person> employee = {
+    //    Person("Архип", "Архипов", "Архипович", "Слесарь"),
+    //    Person("Борис", "Борисов", "Борисович", "Директор"),
+    //    Person("Валентин", "Валентинов", "Валентинович", "Повар"),
+    //    Person("Геннадий", "Геннадьев", "Геннадьевич", "Сварщик"),
+    //    Person("Дмитрий", "Дмитриев", "Дмитриевич", "Повар")
+    //};
+    //std::list<std::string> jobs = {
+    //    "Повар",
+    //    "Директор"
+    //};
+    //auto filtered = matchPersonProfessions(employee, jobs);
+    //for (auto& person : filtered) { std::cout << person << std::endl; }
+
+
+    ////2
+     //std::cout << std::endl;
+    
+     int count = 0;
+    std::cout << "Введите длину последовательности: ";
+    std::cin >> count;
+    std::cout << std::endl;
+    std::string seq = "";
+    std::cout << "Введите последовательность скобок: ";
+    for (int i = 0; i < count; i++)
+    {
+        char sk = '\0';
+        std::cin >> sk;
+        seq += sk;
+    }
+    std::cout << std::endl;
+    std::cout << "Последовательность верна?" << (isValid(seq) ? " Да" : " Нет") << std::endl;
+
+
+    // 
+    //std::cout << std::endl;
+    //int count = 0;
+    //std::cout << "Введите длину последовательности: ";
+    //std::cin >> count;
+    //std::cout << std::endl;
+    //std::string seq = "";
+    //std::cout << "Введите последовательность скобок: ";
+    //for (int i = 0; i < count; i++)
+    //{
+    //    char sk = '\0';
+    //    std::cin >> sk;
+    //    seq += sk;
+    //}
+    //std::cout << std::endl;
+    //std::cout << "Последовательность верна?" << (isValid(seq) ? " Да" : " Нет") << std::endl;
+    //_CrtSetReportMode(_CRT_WARN, _CRTDBG_MODE_DEBUG);
+    //_CrtDumpMemoryLeaks();
+    return 0;
+}
